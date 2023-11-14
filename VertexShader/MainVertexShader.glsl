78452dacae2d4f4f86d41dffec0b56a4cfe6d0c5
Simon P <poto0002@hz.nl>
2023-11-14 20:21:49 +0100

    workshop will end here

--- VertexShader/MainVertexShader.glsl
+++ VertexShader/MainVertexShader.glsl
@@ -1,10 +1,5 @@
 #version 330 core
 
-//	vector
-//	colection of number
-//	can be 1,2,3,4 component vector
-
-//this is input of the vertex shader 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
